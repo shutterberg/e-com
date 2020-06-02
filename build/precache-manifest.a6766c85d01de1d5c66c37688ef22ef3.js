61cede0b60688d645bd1fbe23e9c27b71ac5ac6a
shutterberg <misquithin@gamil.com>
2020-06-02 17:19:03 +0530

    v-1.0.0
    first commit with about shop and cart

---
+++ build/precache-manifest.a6766c85d01de1d5c66c37688ef22ef3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "548064c12c9e1293f796",
+    "url": "/static/css/main.db03464f.chunk.css"
+  },
+  {
+    "revision": "548064c12c9e1293f796",
+    "url": "/static/js/main.f5e75bc5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa296b2c43e840b5ebf4",
+    "url": "/static/js/2.88057c01.chunk.js"
+  },
+  {
+    "revision": "e61ac19a81e4a90208cc711d880c7dda",
+    "url": "/static/media/shopping-cart.e61ac19a.svg"
+  },
+  {
+    "revision": "59741bdcaea0e6b4e3c27e2631800479",
+    "url": "/static/media/luggage.59741bdc.jpg"
+  },
+  {
+    "revision": "7110160dd1c3e266dc664063cf216984",
+    "url": "/index.html"
+  }
+];
